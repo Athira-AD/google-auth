edfd92f8fce67c224e6c969c72f2fc454d596f85
Athira-AD <athiradevraj94@gmail.com>
2022-06-06 18:08:47 +0530

    first commit

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 // Middleware
 app.set('view engine', 'ejs');
@@ -11,8 +11,8 @@ app.get('/',(req, res)=>{
     res.render('index');
 })
 
-app.get('/login', (req, res)=>{
-    res.render('login')
+app.get('/login',(req, res)=>{
+    res.render('login');
 })
 
 app.listen(PORT, ()=>{
